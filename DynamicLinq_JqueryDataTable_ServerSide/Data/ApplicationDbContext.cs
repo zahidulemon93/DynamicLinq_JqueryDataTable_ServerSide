89565ae7c3d79a5c2557050458b3b99091108ce8
Emon <Emon@DESKTOP-98TRJS2>
2021-09-22 18:25:45 +0600

    Add project files.

---
+++ DynamicLinq_JqueryDataTable_ServerSide/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DynamicLinq_JqueryDataTable_ServerSide.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DynamicLinq_JqueryDataTable_ServerSide.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
